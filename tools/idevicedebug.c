21f7c71a424cfaed6b8f995aa6627c08de4b3a63
David O'Donoghue <david@oddgames.com.au>
2019-03-06 15:44:06 +0800

    Update idevicedebug.c
    Copied sleep code from another fork https://github.com/brunoro/libimobiledevice/commit/2727fd508145018ce532bf525bd0b5383ce6b1f3

--- tools/idevicedebug.c
+++ tools/idevicedebug.c
@@ -33,7 +33,18 @@
 
 #ifdef WIN32
 #include <windows.h>
-#define sleep(x) Sleep(x*1000)
+void usleep(__int64 usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER ft;
+
+	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
 #endif
 
 #include <libimobiledevice/installation_proxy.h>
